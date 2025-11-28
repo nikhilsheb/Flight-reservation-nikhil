1e617ac759dee9e1ac478195fd0521bdfbd23d27
root <root@ip-172-31-19-69.ap-south-1.compute.internal>
2025-11-28 06:32:36 +0000

    backend.groovy file added

---
+++ FlightReservationApplication/backend.groovy
@@ -0,0 +1,26 @@
+pipeline{
+    agent any 
+    stages{
+        stage('Pull'){
+            steps{
+                git branch: 'main', url: 'https://github.com/nikhilsheb/Flight-reservation-nikhil.git'
+            }
+        }
+        stage('Build'){
+            steps{
+                
+            }
+        }
+        stage('QA-Test'){
+            steps{
+                       
+            }
+        }
+        stage('Docker-build'){
+            steps{
+                
+            }
+        }
+       
+    }
+}
