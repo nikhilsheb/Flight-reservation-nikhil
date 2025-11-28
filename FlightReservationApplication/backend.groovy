8a5c542479f31fbccb9ddc9fc7ad8e666eb4056d
root <root@ip-172-31-19-69.ap-south-1.compute.internal>
2025-11-28 08:20:45 +0000

    # stages added

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,16 +6,24 @@ pipeline{
                 git branch: 'main', url: 'https://github.com/nikhilsheb/Flight-reservation-nikhil.git'
             }
         }
-        // stage('Build'){
-        //     steps{
-                
-        //     }
-        // }
-        // stage('QA-Test'){
-        //     steps{
+        stage('Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    mvn clean package
+                ''' 
+            }
+        }
+        stage('QA-Test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                    sh '''
+                        cd FlightReservationApplication
+                        mvn sonar:sonar  -Dsonar.projectKey=flight-reservation
+                    '''
                        
-        //     }
-        // }
+            }
+        }
         // stage('Docker-build'){
         //     steps{
                 
